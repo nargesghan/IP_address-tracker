9d19757037a2ef36bce09bc72c853191d1c9680f
Narges Ghanbari <nargesghanbarigorji@gmail.com>
2023-09-26 21:27:53 +0330

    get ip and data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-import { useState } from "react";
-function submit(ev,ip){
-  ev.preventDefault();
-  const [input] = ev.target.children
-    fetch(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=at_wdHaXZyZnmvvZ7vT9y4GXi9wWtUIN&ipAddress=${ip}`
-    )
-      .then((Response) => {
-        return Response.json();
-      })
-      .then((data)=>console.log(data));
-      console.log(ip);
-  }
+
+import Top from "./components/Top";
+import './App.css';
+
 
 function App() {
-const [ip,setIp]=useState('');
+
 
   return (
     <>
     {/* <form onSubmit={(ev)=>submit(ev,ip)}> <input  onChange={(e)=>{setIp(e.target.value)}} type="text" placeholder="8.8.8.8"></input><input type="submit"></input></form> */}
-      
+      <Top/>
     </>
   );
 }
--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./Top.css";
+import { useState } from "react";
+
+function submit(ev,ip){
+  ev.preventDefault();
+  const [input] = ev.target.children
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_wdHaXZyZnmvvZ7vT9y4GXi9wWtUIN&ipAddress=${ip}`
+    )
+      .then((Response) => {
+        return Response.json();
+      })
+      .then((data)=>console.log(data));
+      console.log(ip);
+  }
 
 const Top = () => {
-  return (
-    <div>Top</div>
-  )
-}
+  const [ip,setIp]=useState('');
 
-export default Top
+  return (
+    <div className="container">
+      <div className="topImage">
+        <h1 className="title">IP Address Tracker</h1>
+        <form onSubmit={(ev)=>submit(ev,ip)}>
+          <input
+            className="input"
+            placeholder="search for any IP address or domain"
+            onChange={(e)=>{setIp(e.target.value)}}
+          ></input>
+          <button className="enter">&gt;</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Top;
